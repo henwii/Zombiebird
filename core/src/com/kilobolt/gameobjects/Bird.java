51426734c16578940be91267fd1fb5fa64373a28
henwii <henri.gustavsson@live.se>
2015-10-15 20:29:50 +0200

    Fixed grtar

--- core/src/com/kilobolt/gameobjects/Bird.java
+++ core/src/com/kilobolt/gameobjects/Bird.java
@@ -26,7 +26,29 @@ public class Bird {
         if(velocity.y > 200){
             velocity.y = 200;
         }
+
         position.add(velocity.cpy().scl(delta));
+
+        if(velocity.y < 0){
+            rotation -= 600*delta;
+
+            if(rotation < -20){
+                rotation = -20;
+            }
+        }
+
+        if(isFalling()){
+            rotation += 480*delta;
+            if(rotation > 90){
+                rotation = 90;
+            }
+        }
+    }
+    public boolean isFalling(){
+        return velocity.y > 110;
+    }
+    public boolean shouldntFlap(){
+        return velocity.y > 70;
     }
     public void onClick(){
         velocity.y = -140;
